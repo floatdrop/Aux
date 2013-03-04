cddf5883016f52a6794f2fd357cb71d2627e9eac
Vsevolod <floatdrop@gmail.com>
2013-03-04 21:04:02 +0600

    Pre-nodejs wipe

--- backend/__init__.py
+++
@@ -1,7 +0,0 @@
-from engine import world
-
-from flask import Flask
-app = Flask(__name__)
-world.logger = app.logger
-
-import backend.views
--- backend/views.py
+++
@@ -1,6 +0,0 @@
-from backend import app
-
-@app.route('/')
-def hello_world():
-    app.logger.info("Hello Logger!")
-    return 'Hello World!'
