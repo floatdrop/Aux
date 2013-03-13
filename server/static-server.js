182e331245e83d0fe57d7a218ca53dfe6a4bb1d7
Dmitry Zyr <zyrianovdv@gmail.com>
2013-03-13 20:29:53 +0600

    Разбиение на файлы

--- server/static-server.js
+++ server/static-server.js
@@ -11,23 +11,26 @@ var mimeTypes = {
     "js": "text/javascript",
     "css": "text/css"};
 
-http.createServer(function(req, res) {
-    var uri = url.parse(req.url).pathname;
-    if (uri=="/") {
-        uri = "/index.html";
-    }
-    var filename = path.join(process.cwd() + "../client/", uri);
-    path.exists(filename, function(exists) {
-        if(!exists) {
-            console.log("not exists: " + filename);
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.write('404 Not Found\n');
-            res.end();
-            return;
-        }
-        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
-        res.writeHead(200, {'Content-Type':mimeType});
-        var fileStream = fs.createReadStream(filename);
-        fileStream.pipe(res);
-    });
-}).listen(80);
+function start(port){
+    http.createServer(function(req, res) {
+        
+        var uri = url.parse(req.url).pathname;
+        if (uri == '/') uri = "index.html";
+        var extname = path.extname(uri).split(".")[1];
+        var filename = path.join("../client", uri);
+
+        path.exists(filename, function(exists) {
+            if(!exists) {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('404 Not Found\n');
+                return;
+            }
+
+            res.writeHead(200, mimeTypes[extname]);
+            var fileStream = fs.createReadStream(filename);
+            fileStream.pipe(res);
+        });
+    }).listen(port);
+}
+
+exports.start = start;
