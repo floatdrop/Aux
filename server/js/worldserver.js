96bb6c9254413779e6199c9f5f115cb68fd130a6
floatdrop <floatdrop@gmail.com>
2013-03-14 16:13:54 +0600

    more refactoring

--- server/js/worldserver.js
+++ server/js/worldserver.js
@@ -3,5 +3,15 @@ var cls = require("./lib/class"),
 	log = require("log");
 
 module.exports = World = cls.Class.extend({
-	
+	init: function() {
+		this.ups = 50;
+	},
+	run: function() {
+		setInterval(function () { 
+			// Here we need to tick box2d
+			// But this code is wrong in many ways:
+			// game.update(webSocket.getConnection());
+			log.debug("Tick");
+		},	1000 / this.ups);
+	}
 });
