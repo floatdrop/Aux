3901a6b367319f4a1b1a66a9b5e758be02cd834a
floatdrop <floatdrop@gmail.com>
2013-03-19 18:47:53 +0600

    mouse and keyboard handled in main.js. Proceeding to game.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,37 @@
-define(['jquery', 'app'], function($, App) {
+define(['jquery', 'game'], function($, Game) {
+
+	var canvas = document.getElementById("gamecanvas");
+	var game = new Game();
+	game.setCanvas(canvas);
+
+	$(document).mousemove(function(event) {
+		var gamePos = $('#gamecanvas').offset(),
+				mouse = game.mouse;
+
+		mouse.x = event.pageX - gamePos.left;
+    mouse.y = event.pageY - gamePos.top;
+
+  	game.movecursor();
+  });
+
+	$(document).bind("keydown", function(e) {
+			var key = e.which;
+			if (key === 87) { // W
+				game.moveUp();
+				return false;
+			}
+			if (key === 83) { // S
+				game.moveDown();
+				return false;
+			}
+			if (key === 65) { // A
+				game.moveLeft();
+				return false;
+			}
+			if (key === 68) { // D
+				game.moveRight();
+				return false;
+			}
+	});
 
 });
