85f547dfca2b11d8f2ee3beb0883d5627a916334
floatdrop <floatdrop@gmail.com>
2013-04-04 01:26:38 +0600

    fix sprites

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1,16 +1,18 @@
-define(['./lib/text!../sprites/player.json',
+define(['lib/underscore',
+	'./lib/text!../sprites/player.json',
 	'./lib/text!../sprites/trie.json',
 	'./lib/text!../sprites/stone.json',
 	'./lib/text!../sprites/stone2.json',
 	'./lib/text!../sprites/stone3.json',
 	'./lib/text!../sprites/stone4.json',
 	'./lib/text!../sprites/stump.json',
-	'./lib/text!../sprites/empty.json',
-	'lib/underscore'], function (_) {
+	'./lib/text!../sprites/empty.json'], function (_) {
 
 	var sprites = {};
 
-	_.each(arguments, function (spriteJson) {
+	var args = arguments.splice(1, 1);
+
+	_.each(args, function (spriteJson) {
 		var sprite = JSON.parse(spriteJson);
 		sprites[sprite.id] = sprite;
 	});
