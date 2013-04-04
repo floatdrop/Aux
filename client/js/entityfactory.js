3d141716e6e503eb5def9b878b70b7ff4c28d37d
DmitryZyr <zyrianovdv@gmail.com>
2013-04-04 19:48:58 +0400

    исправлены ошибки

--- client/js/entityfactory.js
+++ client/js/entityfactory.js
@@ -3,7 +3,8 @@
 define(['player', 'commonEntity'], function (Player, CommonEntity) {
 	var EntityFactory = {};
 
-	EntityFactory.createEntity = function (kind, id, name) {
+	EntityFactory.createEntity = function (entity_info, name) {
+		var kind = entity_info.kind;
 		if (!kind) {
 			console.log.error("kind is undefined", true);
 			return;
@@ -13,17 +14,17 @@ define(['player', 'commonEntity'], function (Player, CommonEntity) {
 			throw new Error(kind + " is not a valid Entity type");
 		}
 
-		return EntityFactory.builders[kind](id, name);
+		return EntityFactory.builders[kind](entity_info, name);
 	};
 
 	EntityFactory.builders = [];
 
-	EntityFactory.builders[Constants.Types.Entities.PLAYER] = function (id, name) {
-		return new Player(id, name);
+	EntityFactory.builders[Constants.Types.Entities.PLAYER] = function (entity_info, name) {
+		return new Player(entity_info.id, name);
 	};
 
-	EntityFactory.builders[Constants.Types.Entities.CommonEntity] = function (id, name) {
-		return new CommonEntity(id, name);
+	EntityFactory.builders[Constants.Types.Entities.CommonEntity] = function (entity_info) {
+		return new CommonEntity(entity_info);
 	};
 
 	return EntityFactory;
