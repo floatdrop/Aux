bfb59ce05f640fb167f0a2b429491a070444dba3
DmitryZyr <zyrianovdv@gmail.com>
2013-04-11 21:17:25 +0400

    ЗАпилил новую карту

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1,13 +1,19 @@
 /* global _ */
 
 define(['./lib/text!../sprites/player.json',
-	'./lib/text!../sprites/trie.json',
+	'./lib/text!../sprites/tree.json',
 	'./lib/text!../sprites/stone.json',
 	'./lib/text!../sprites/stone2.json',
 	'./lib/text!../sprites/stone3.json',
 	'./lib/text!../sprites/stone4.json',
 	'./lib/text!../sprites/stump.json',
-	'./lib/text!../sprites/empty.json'], function () {
+	'./lib/text!../sprites/empty.json',
+	'./lib/text!../sprites/pillar.json',
+	'./lib/text!../sprites/pillar2.json',
+	'./lib/text!../sprites/signpost.json',
+	'./lib/text!../sprites/stone5.json',
+	'./lib/text!../sprites/stone6.json',
+	'./lib/text!../sprites/well.json'], function () {
 
 	var sprites = {};
 
