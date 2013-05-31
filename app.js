bd91a5e38e0280db4cf08f9554c763472fda8892
Vsevolod Strukchinsky <floatdrop@gmail.com>
2013-05-31 13:15:56 +0400

    It has begun!

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-var fs = require('fs'),
-	Server = require('./server/js/server');
 
-function getConfigFile(path, callback) {
-	fs.readFile(path, 'utf8', function (err, json_string) {
-		if (err) {
-			console.error("Could not open config file: ", err.path);
-			callback(null);
-		} else {
-			callback(JSON.parse(json_string));
-		}
-	});
-}
+GLOBAL.config = require('./config.js');
+GLOBAL.Class = require('./server/vendor/class.js').Class;
+GLOBAL._ = require('lodash');
+GLOBAL.log = require('./server/log.js')
 
-var defaultConfigPath = './server/config.json',
-	customConfigPath = './server/config_local.json';
+var Server = require('./server');
 
-process.argv.forEach(function (val, index) {
-	if (index === 2) {
-		customConfigPath = val;
-	}
-});
-
-getConfigFile(customConfigPath, function (customConfig) {
-	if (customConfig) {
-		var server = new Server(customConfig);
-		server.start();
-	} else {
-		getConfigFile(defaultConfigPath, function (defaultConfig) {
-			if (defaultConfig) {
-				var server = new Server(defaultConfig);
-				server.start();
-			} else {
-				console.error("Server cannot start without any configuration file.");
-				process.exit(1);
-			}
-		});
-	}
-});
+var server = new Server();
+server.start();
